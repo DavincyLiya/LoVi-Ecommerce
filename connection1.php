73d89bc26bdc782bfcfa71559b9c8033f86a3423
DavincyLiya <davincya27@gmail.com>
2025-08-31 13:07:43 +0530

    connection1.php

--- connection1.php
+++ connection1.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "lovi";
+$dbname     = "lovi_ecommerce";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
